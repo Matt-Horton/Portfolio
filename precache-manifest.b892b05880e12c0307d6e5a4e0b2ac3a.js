62a52a0f39317e9fa641520e7e16cca74f46b9f9
Matty-Horton <mthorton1@sheffield.ac.uk>
2018-12-27 19:57:17 +0000

    Updates

---
+++ precache-manifest.b892b05880e12c0307d6e5a4e0b2ac3a.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "91fd24a1b08594816f2a0e569f37531a",
+    "url": "/Portfolio/static/media/timewise.91fd24a1.png"
+  },
+  {
+    "revision": "7f800bdb56d2742bc155518ef408dece",
+    "url": "/Portfolio/static/media/swwop.7f800bdb.png"
+  },
+  {
+    "revision": "671a599d842106ec8c1cd911f043e9de",
+    "url": "/Portfolio/static/media/portfolio.671a599d.png"
+  },
+  {
+    "revision": "21668e67c85a403411ef391b87db54c2",
+    "url": "/Portfolio/static/media/laptop-background1.21668e67.jpg"
+  },
+  {
+    "revision": "3b96116072eb85f5a3a2",
+    "url": "/Portfolio/static/js/runtime~main.3b961160.js"
+  },
+  {
+    "revision": "47eeb4300c11c9063185",
+    "url": "/Portfolio/static/js/main.47eeb430.chunk.js"
+  },
+  {
+    "revision": "20f23405ea3204c96367",
+    "url": "/Portfolio/static/js/1.20f23405.chunk.js"
+  },
+  {
+    "revision": "47eeb4300c11c9063185",
+    "url": "/Portfolio/static/css/main.c808eef6.chunk.css"
+  },
+  {
+    "revision": "5d160f1960d313511dae27b6ec8768dc",
+    "url": "/Portfolio/index.html"
+  }
+];
